793e1c2d95969eaaff444ef3c8036189e35d0b29
2xandrade <r.2x.andrade@gmail.com>
2026-03-16 22:16:38 -0300

    adicionando TarefaServices ao repo

---
+++ demo/src/main/java/com/tarefas/demo/services/TarefaServices.java
@@ -0,0 +1,30 @@
+package com.tarefas.demo.services;
+
+import com.tarefas.demo.model.TarefaModel;
+import com.tarefas.demo.repositories.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TarefaServices {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    public List<TarefaModel> findAll() { return tarefaRepository.findAll() ;}
+
+    public TarefaModel criarTarefa(TarefaModel tarefaModel) { return tarefaRepository.save(tarefaModel);}
+
+    public Optional<TarefaModel> buscarId(Long id) { return tarefaRepository.findById(id); }
+
+    public TarefaModel atualizar(Long id, TarefaModel tarefaModel) {
+        TarefaModel model =  tarefaRepository.findById(id).get();
+        model.setDescricao(tarefaModel.getDescricao());
+        model.setDataVencimento(tarefaModel.getDataVencimento());
+        model.setConcluida(tarefaModel.getConcluida());
+        return tarefaRepository.save(model);
+    }
+
+    public void excluir(Long id) { tarefaRepository.deleteById(id); }
+}
